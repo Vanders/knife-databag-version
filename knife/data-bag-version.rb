c32e1fdfa2a877f8555d5b613e8fc397a2f8b347
Kristian Van Der Vliet <kvandervliet@dyn.com>
2013-11-08 12:58:04 +0000

    Add knife plugin

---
+++ knife/data-bag-version.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require 'databag-version'
+
+module Dyn
+  module Knife
+
+    # Version all data bags items
+
+    class DataBagVersion < Chef::Knife
+
+      banner 'knife data bag version'
+
+      def run
+        DatabagVersion.process_all
+      end
+    end
+
+  end
+end
